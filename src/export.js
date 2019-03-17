d78ba176aef5cd5db340f9698f3e8c11af1f4cd4
falstack <icesilt@outlook.com>
2019-03-17 10:25:37 +0800

    add: export.js

---
+++ src/export.js
@@ -0,0 +1,9 @@
+import Waterfall from './Waterfall'
+import WaterfallSlot from './WaterfallSlot'
+
+export default {
+  Waterfall: Waterfall,
+  WaterfallSlot: WaterfallSlot,
+  waterfall: Waterfall,
+  waterfallSlot: WaterfallSlot
+}
